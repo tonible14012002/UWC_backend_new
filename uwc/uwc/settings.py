db5253a77e12d94a468f78c5f9fd0e4dc82da0f9
duyvt6663 <93929554+duyvt6663@users.noreply.github.com>
2022-11-26 19:10:58 +0700

    update swagger

--- uwc/uwc/settings.py
+++ uwc/uwc/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'account',
     'map',
-
+    'rest_framework_swagger',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
